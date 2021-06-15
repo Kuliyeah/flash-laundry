cdbbe17e0501eb9a06d7e535f145591f6a580dea
lovanto <rifkylovanto@gmail.com>
2021-06-15 19:23:05 +0700

    center screen frame

--- src/View/InputTransaksi.java
+++ src/View/InputTransaksi.java
@@ -331,6 +331,7 @@ public class InputTransaksi extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnSaveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSaveActionPerformed
